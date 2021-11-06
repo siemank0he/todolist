0a9a9587e0ae3d25e0c4cf8ac4cc22be39a237d4
siemank0he <79216728+siemank0he@users.noreply.github.com>
2021-11-06 16:56:16 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
+const taskArr = [];
+
 const addTaskButton = document.querySelector(".addTask");
 const taskTitle = document.querySelector(".taskText");
 const taskType = document.querySelector(".taskInput input");
 const taskBox = document.querySelector(".task");
-const taskDeleteBtn = document.querySelector(".taskDelete");
-const mainSection = document.querySelector(".main");
+const mainSection = document.querySelector(".tasks");
 
-const taskArr = [];
+const deleteTaskByKey = (e) => {
+    const index = e.target.parentNode.dataset.key;
+    console.log(index);
+    taskArr.splice(index, 1);
+    renderTasks();
+}
 
 const addTaskFun = () => {
     const inputValue = taskType.value;
@@ -13,16 +19,16 @@ const addTaskFun = () => {
         taskType.style.border = "4px solid red";
         taskType.placeholder = "You didn't enter any task! :(";
     } else {
-        taskArr.push(inputValue);
         taskType.value = "";
         taskType.style.border = "0";
         taskType.placeholder = "add a task";
-        renderTask();
+        createTask(inputValue);
+        renderTasks();
     }
     
 }
 
-const renderTask = () => {
+const createTask = (inputValue) => {
     const task = document.createElement("div");
     const taskText = document.createElement("span");
     const deleteBtn = document.createElement("span");
@@ -31,7 +37,17 @@ const renderTask = () => {
     deleteBtn.classList.add("taskDelete");
     task.appendChild(taskText);
     task.appendChild(deleteBtn);
-    mainSection.appendChild(task);
+    taskText.textContent = inputValue;
+    taskArr.push(task);
+    task.querySelector(".task .taskDelete").addEventListener("click", deleteTaskByKey);
+}
+
+const renderTasks = () => {
+    mainSection.textContent = "";
+    taskArr.forEach((item, key) => {
+        item.dataset.key = key;
+        mainSection.appendChild(item);
+    })
 }
 
 addTaskButton.addEventListener("click", addTaskFun);
